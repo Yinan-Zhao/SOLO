7a131856f9fded801b710a99d0668888e7ba2d3c
Zhao <alexzhao@cs.utexas.edu>
2020-04-28 16:36:37 -0500

    Initial commit

--- mmdet/models/detectors/solo_att.py
+++ mmdet/models/detectors/solo_att.py
@@ -23,7 +23,7 @@ class SOLOAtt(SingleStageInsDetector):
                       gt_labels,
                       gt_bboxes_ignore=None,
                       gt_masks=None):
-    	pdb.set_trace()
+        pdb.set_trace()
         x = self.extract_feat(img)
         loss_inputs = (x, gt_bboxes, gt_labels, gt_masks, img_metas, self.train_cfg)
         losses = self.bbox_head.loss(
@@ -31,8 +31,8 @@ class SOLOAtt(SingleStageInsDetector):
         return losses
 
     def simple_test(self, img, img_meta, rescale=False):
-    	with torch.no_grad():
-	        x = self.extract_feat(img)
-	        seg_inputs = (x, img_meta, self.test_cfg, rescale)
-	        seg_result = self.bbox_head.get_seg(*seg_inputs)
-	        return seg_result  
+        with torch.no_grad():
+            x = self.extract_feat(img)
+            seg_inputs = (x, img_meta, self.test_cfg, rescale)
+            seg_result = self.bbox_head.get_seg(*seg_inputs)
+            return seg_result  
