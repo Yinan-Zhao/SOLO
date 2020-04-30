a25ce4de83124cc593c49dee74bcddf0cc35044a
Zhao <alexzhao@cs.utexas.edu>
2020-04-29 23:20:34 -0500

    Initial commit

--- configs/solo/solo_att_r101_fpn_4gpu_3x.py
+++ configs/solo/solo_att_r101_fpn_4gpu_3x.py
@@ -94,8 +94,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    imgs_per_gpu=2,
-    workers_per_gpu=1,
+    imgs_per_gpu=4,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2017.json',
